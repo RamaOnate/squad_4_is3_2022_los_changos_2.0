22d09ac8be9ad218643c907122d6d7657a5cc7ed
RamaOnate <ramadelarbolito@gmail.com>
2022-06-10 15:44:58 -0300

    check if backendSession now works in caps

--- features/license_definitions/connection.js
+++ features/license_definitions/connection.js
@@ -1,5 +1,5 @@
 const { Given } = require('@cucumber/cucumber')
-const BackendSession = require('../backendSession');
+const BackendSession = require('../BackendSession');
 
 Given('un empleado ingresa a la aplicacion', function () {
   this.backendSession = new BackendSession ("localhost", 3000)
