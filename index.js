3cff4e56979a4198bea941fa36c493b6502ab1a4
Lucas Ariel Saavedra <46687572+lucasSaavedra123@users.noreply.github.com>
2022-06-11 13:51:03 -0300

    Swagger agregado (#19)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const swaggerDocument = require('./swagger-output.json');
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.u2sff.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
@@ -18,5 +21,7 @@ app.use('/licenses', licenseRouter)
 
 const hourRouter = require('./routes/hours')
 app.use('/hours', hourRouter)
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(process.env.PORT || 5000, () => console.log('Server Started'))
