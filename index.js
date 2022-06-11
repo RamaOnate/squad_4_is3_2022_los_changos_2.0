034879b5aa785416332ec8b148d096ef80c5cf2b
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-06-11 14:02:06 -0300

    code updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const swaggerUi = require('swagger-ui-express');
 
 const swaggerDocument = require('./swagger-output.json');
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.u2sff.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
