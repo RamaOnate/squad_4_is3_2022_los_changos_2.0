cb96523ff9da9342cdd36cdd0d4ca293b77cffc2
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-06-11 22:49:03 -0300

    code updated

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const notify_employee = require('./utils/notify_employee')
+const CronJob = require('cron').CronJob
+
+//Every day at 18:00, employees that did not upload hours will be notified
+const job = new CronJob('0 18 * * *', () => { 
+    notify_employee({"legajo":1,"Nombre":"Mario","Apellido":"Mendoza"})
+}
+)
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
@@ -25,4 +33,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+job.start()
+console.log("Server listening...")
 app.listen(process.env.PORT || 5000, () => console.log('Server Started'))
