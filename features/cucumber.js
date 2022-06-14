d16e509fa47b6040973cfb6ef6765b1a9398d6c5
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-06-13 21:55:31 -0300

    code updated

--- features/cucumber.js
+++ features/cucumber.js
@@ -1,10 +1,3 @@
-const {Before} = require('@cucumber/cucumber');
-const { spawn } = require('child_process');
-
-Before({name: "[CUCUMBER] Start Server..."}, function () {
-    spawn('node', ['index.js']);
-});
-
 module.exports = {
     default: `--format-options '{"snippetInterface": "synchronous"}'`
 }
