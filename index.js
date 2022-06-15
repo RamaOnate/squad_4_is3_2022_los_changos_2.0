31b306ea65c29a180f257da3636f6f509de4d061
Lucas Ariel Saavedra <46687572+lucasSaavedra123@users.noreply.github.com>
2022-06-15 20:56:45 -0300

    Envio de notificaciones (#21)
    * mail service added
    
    * listorti
    
    * code updated
    
    * updated
    
    * protonmail package removed
    
    * code updated
    
    * code updated
    
    * code updated
    
    * code updated
    
    * Code updated
    
    * code updated

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+require('dotenv').config({path: __dirname + '/.env'})
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger-output.json');
+const notify_employees = require('./utils/notify_employees')
+const CronJob = require('cron').CronJob
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
+
+//Every day at 18:00, employees that did not upload hours will be notified
+const job = new CronJob('0 17 * * 1-5', () => {
+    console.log("Notificando empleados...")
+    notify_employees()
+    console.log("Notificaciones enviadas...")
+})
 
 app.use(express.json())
 
@@ -21,6 +30,10 @@ app.use('/licenses', licenseRouter)
 const hourRouter = require('./routes/hours')
 app.use('/hours', hourRouter)
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
+job.start()
+console.log("Server listening...")
 app.listen(process.env.PORT || 5000, () => console.log('Server Started'))
---
+++ utils/notify_employees.js
@@ -0,0 +1,56 @@
+require('dotenv').config({path: __dirname + '../../.env'})
+const notify_employee = require('./notify_employee')
+const HourModel = require('../models/hour')
+const request = require('request')
+
+//https://flaviocopes.com/how-to-determine-date-is-today-javascript/
+const isToday = (someDate) => {
+  const today = new Date()
+  return someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
+}
+
+const hourOfToday = (document) => {
+  return isToday(document.created)
+}
+
+const employeeLoadedHour = (employee, hours) => {
+  for(let i = 0; i < hours.length; i++){ 
+
+    if(hours[i].hourAssignee == employee.legajo){
+      return true
+    }
+  }
+
+  return false
+
+}
+
+async function notify_employees(){
+  // EMPLEADOS - EMPLEADOS_QUE_CARGARON -> EMPLEADO_QUE_NO_CARGARON. :)
+  const options = {
+    url: process.env.RESOURCES_DATABASE,
+    method: 'GET',
+    json: true,
+  }
+
+  request(options, async (err, res, body) => {
+    if(res != undefined){
+      var employees = body;
+      var hours_loaded = await HourModel.find({}).exec();
+      var hours_loaded_today = hours_loaded.filter(hourOfToday);
+
+      for(let i = 0; i < employees.length; i++){ 
+
+        if(!employeeLoadedHour(employees[i], hours_loaded_today)){
+          notify_employee(employees[i])
+        }
+      }      
+
+    }
+  })
+
+}
+
+module.exports = notify_employees
