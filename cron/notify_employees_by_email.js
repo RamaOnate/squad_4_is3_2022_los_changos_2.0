e754e6dd649aa7a2bec9583904b823834ce13a44
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-06-16 20:53:43 -0300

    cron directory addwed

--- cron/notify_employees_by_email.js
+++ cron/notify_employees_by_email.js
@@ -1,7 +1,13 @@
-//require('dotenv').config({path: __dirname + '../../.env'})
-const notify_employee = require('./notify_employee')
+require('dotenv').config({path: __dirname + '/../.env'})
+const notify_employee = require('../utils/notify_employee')
 const HourModel = require('../models/hour')
 const request = require('request')
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
 //https://flaviocopes.com/how-to-determine-date-is-today-javascript/
 const isToday = (someDate) => {
@@ -27,30 +33,24 @@ const employeeLoadedHour = (employee, hours) => {
 
 }
 
-async function notify_employees(){
-  // EMPLEADOS - EMPLEADOS_QUE_CARGARON -> EMPLEADO_QUE_NO_CARGARON. :)
-  const options = {
-    url: process.env.RESOURCES_DATABASE,
-    method: 'GET',
-    json: true,
-  }
+const options = {
+  url: process.env.RESOURCES_DATABASE,
+  method: 'GET',
+  json: true,
+}
 
-  request(options, async (err, res, body) => {
-    if(res != undefined){
+request(options, async (err, res, body) => {
+  if(res != undefined){
+    try{
       var employees = body;
       var hours_loaded = await HourModel.find({}).exec();
       var hours_loaded_today = hours_loaded.filter(hourOfToday);
 
       for(let i = 0; i < employees.length; i++){ 
-
-        if(!employeeLoadedHour(employees[i], hours_loaded_today)){
-          notify_employee(employees[i])
-        }
-      }      
-
+        if(!employeeLoadedHour(employees[i], hours_loaded_today)){notify_employee(employees[i])}
+      }
+    } catch (error) {
+        console.log(error)
     }
-  })
-
-}
-
-module.exports = notify_employees
+  }
+})
--- test_mail.js
+++ test_mail.js
@@ -1,2 +1,2 @@
 const notify_employee = require('./utils/notify_employee')
-a = notify_employee({"legajo":1,"Nombre":"Mario","Apellido":"Mendoza"})
+notify_employee({"legajo":1,"Nombre":"Mario","Apellido":"Mendoza"})
