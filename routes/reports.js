14d54ce5408cc89a0d28b7a74ec3a56964cbbba9
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2022-06-17 15:59:33 -0300

    report fixed

--- routes/reports.js
+++ routes/reports.js
@@ -3,14 +3,47 @@ const router = express.Router()
 const request = require('request')
 const Hour = require('../models/hour')
 
+function pick_employee_by_id(employees, id) {
+
+  for (i = 0; i < employees.length; i++) {
+    if (employees[i].legajo == id) {
+      return employees[i]
+    }
+  }
+
+  return undefined
+}
+
 router.get('/person/:id', async (req, res) => {
   try {
 
     request({ url: process.env.RESOURCES_DATABASE, method: 'GET', json: true }, async (err, res2, body) => {
-      hour = await Hour.find({hourAssignee:req.params.id})
-      result = hour.map(anHour => anHour.duration);
-      number_of_hours_worked = result.reduce((previousHour, currentHour) => previousHour + currentHour, 0);
-      res.status(200).json({total_hours_worked: number_of_hours_worked})
+      employee_report = pick_employee_by_id(body, req.params.id)
+
+      if (employee_report == undefined) {
+        res.status(500).json({ message: "Employee doesnt exist" })
+      }
+      else {
+        hour = await Hour.find({ hourAssignee: req.params.id })
+
+        if (hour.length > 0) {
+          result = hour.map(anHour => anHour.duration);
+          number_of_hours_worked = result.reduce((previousHour, currentHour) => previousHour + currentHour, 0);
+
+          if(!isNaN(number_of_hours_worked)){
+            employee_report.total_hours_worked = number_of_hours_worked
+          }
+          else{
+            employee_report.total_hours_worked = 0
+          }
+
+        }
+        else {
+          employee_report.total_hours_worked = 0
+        }
+
+        res.status(200).json(employee_report)
+      }
     })
 
   } catch (err) {
